1deb022c2d95b6f3f639a35f27274543219ae008
James Okutoyi <okutoyijemo@gmail.com>
2026-03-13 09:52:01 +0300

    Initialize workflow

---
+++ src/test/java/com/example/DistributedKafkaOrderProcessing/DistributedKafkaOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DistributedKafkaOrderProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedKafkaOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
